cd3e87866972c2450383b2ed6a7986a71694b6b6
shakesharjahan601-lab <shakesharjahan601@gmail.com>
2026-06-23 14:40:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = 'editor-pro-v1';
+const CACHE_NAME = 'editor-base-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+  'index.html',
+  'manifest.json'
 ];
 
-// Install and save to phone memory
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-// Clean up old memory if we update the app later
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
@@ -25,7 +24,6 @@ self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
-// Load from offline memory if no internet
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
